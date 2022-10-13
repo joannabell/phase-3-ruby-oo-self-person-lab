5d9cb26aff164ea6f2c6a263bfdffa65a2012da9
Joanna Bell <jjbdigital@gmail.com>
2022-10-13 10:41:16 -0500

    commit tree f7af0144dd5e93abf7be9b2b9cf8a0e97fb61432

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,53 @@
-# your code goes here
+require 'pry'
+
+class Person
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(value)
+        @happiness = if value > 10
+            10
+        elsif value < 0
+            0
+        else 
+            value
+        end
+    end      
+
+    def hygiene=(value)
+        @hygiene = if value > 10
+            10
+        elsif value < 0
+            0
+        else 
+            value
+        end
+    end      
+
+    def happy?
+        self.happiness > 7
+    end
+
+    def clean?
+        self.hygiene > 7
+    end
+
+    def get_paid
+        self.bank_account += amount
+        "all about the Benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+    end
+
+end
+
+new_person = Person.new("Joanna")
